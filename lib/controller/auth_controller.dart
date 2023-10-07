f0cc530a519b4742dc631b7df811d0c325ae94df
Sabin Poudel <sabinpoudel6969@gmail.com>
2023-10-07 13:18:13 +0530

    removed screen util package, implemented login feature, and forgot password

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,16 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:personalmeetingschedulingplatform/utils/flutter_toast_message.dart';
+import 'package:personalmeetingschedulingplatform/utils/reuseable_dialog_box.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
 class AuthController {
-  static signInUser(String email, String password) async {
+  static signInUser(String email, String password, BuildContext context) async {
+    ReusableDiaglogBox.dialgoBox(context);
     auth
         .signInWithEmailAndPassword(email: email, password: password)
-        .then((value) {
-      debugPrint("Loading");
+        .then((value) {})
+        .onError(
+      (error, stackTrace) {
+        ReusableDiaglogBox.hideLoadingDialogBox(context);
+        if (error is FirebaseAuthException) {
+          ToastMessage.showToastMessage(error.code.toString());
+        } else {
+          ToastMessage.showToastMessage("An unknown error occured");
+        }
+      },
+    );
+  }
+
+  static sendResetLink(String email, BuildContext context) async {
+    await auth.sendPasswordResetEmail(email: email).then((value) {
+      ToastMessage.showToastMessage("Check email for reset link");
     }).onError((error, stackTrace) {
-      debugPrint("An error occured");
+      ToastMessage.showToastMessage("An error occured");
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:personalmeetingschedulingplatform/provider/remember_me_provider.dart';
 import 'package:personalmeetingschedulingplatform/provider/show_password_provider.dart';
+import 'package:personalmeetingschedulingplatform/views/forgot_password_screen.dart';
 import 'package:personalmeetingschedulingplatform/views/login_page.dart';
 import 'package:provider/provider.dart';
 
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
         initialRoute: "login",
         routes: {
           "login": (context) => const LoginPage(),
+          "fogotpassword": (context) => const ForgotScreenPage(),
         },
       ),
     );
