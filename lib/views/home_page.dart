ec3f1daffa01e2a8263d81c993244769a4f70f4f
Sabin Poudel <sabinpoudel6969@gmail.com>
2023-10-07 18:37:27 +0530

    made bot nav bar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personalmeetingschedulingplatform/controller/auth_controller.dart';
 import 'package:personalmeetingschedulingplatform/controller/url_laucher.dart';
-import 'package:personalmeetingschedulingplatform/utils/ask_for_logout_dialog.dart'; // Import this for date formatting
+import 'package:personalmeetingschedulingplatform/utils/ask_for_logout_dialog.dart';
+import 'package:personalmeetingschedulingplatform/views/meeting_page.dart';
+import 'package:personalmeetingschedulingplatform/views/profile_page.dart'; // Import this for date formatting
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key});
@@ -13,14 +15,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   final stream = FirebaseFirestore.instance
       .collection("usersData")
       .doc(auth.currentUser!.uid)
       .snapshots();
+  PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
     // Get the current time for greeting
-    String greeting = getGreeting();
+    // String greeting = getGreeting();
 
     return Scaffold(
       appBar: AppBar(
@@ -42,15 +46,16 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             UserAccountsDrawerHeader(
               accountName: StreamBuilder(
-                  stream: stream,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const LinearProgressIndicator();
-                    } else {
-                      final data = snapshot.data!.get("name");
-                      return Text(data);
-                    }
-                  }),
+                stream: stream,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const LinearProgressIndicator();
+                  } else {
+                    final data = snapshot.data!.get("name");
+                    return Text(data);
+                  }
+                },
+              ),
               accountEmail: Text(auth.currentUser!.email ?? "loading"),
               currentAccountPicture: const CircleAvatar(
                 radius: 40,
@@ -76,91 +81,50 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              greeting, // Display the greeting
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              DateFormat.yMMMMd()
-                  .format(DateTime.now()), // Display today's date
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 16), // Add some spacing
-
-            // Product planning card
-            Card(
-              elevation: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Product Planning',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const Text(
-                      'Description of the product planning goes here.',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            // Handle invite button press
-                          },
-                          icon: const Icon(Icons.add),
-                          label: const Text('Invite'),
-                        ),
-                        const Icon(Icons.more_vert), // Add your end icon here
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            "addmeeting",
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                // Handle drawer button press
-                Scaffold.of(context).openDrawer();
-              },
+      body: PageView(
+        controller: pageController,
+        children: const [
+          MeetingPage(),
+          ProfilePage(),
+        ],
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          pageController.jumpToPage(value);
+          currentIndex = value;
+          setState(() {});
+        },
+        selectedItemColor: Colors.white,
+        showSelectedLabels: true,
+        unselectedItemColor: Colors.black,
+        backgroundColor: Colors.orange,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.grid_view_outlined,
             ),
-            IconButton(
-              icon: const Icon(Icons.account_circle),
-              onPressed: () {
-                // Handle my account button press
-              },
-            ),
-          ],
-        ),
+            label: "Meetings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "profile",
+          )
+        ],
       ),
     );
   }
