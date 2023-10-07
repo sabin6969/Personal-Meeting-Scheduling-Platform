88997d62f5df1935182fb8fd6345a998a5f1c22c
Sandesh157-Th <ssahi563@rku.ac.in>
2023-10-07 15:34:31 +0530

    splash screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,21 +1,32 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, use_build_context_synchronously, unused_element
 
 import 'package:flutter/material.dart';
+import 'package:personalmeetingschedulingplatform/views/login_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(width: MediaQuery.sizeOf(context).width,
-      height: MediaQuery.sizeOf(context).height,
-      child: Column(children: [
-        Center(child: Image(image: AssetImage("asset/images/Glogo.png")),)
+    Future<void> delayFunction() async {
+      await Future.delayed(Duration(seconds: 1));
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    }
 
-      ]),
-      
-      
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 350, horizontal: 80),
+            child: Image(image: AssetImage("asset/images/Glogo.png")),
+          )
+        ]),
       ),
     );
   }
