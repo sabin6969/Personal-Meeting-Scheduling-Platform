b9faa0005fab5cd74cbe01fea8e65d14888d792e
Sandesh157-Th <ssahi563@rku.ac.in>
2023-10-07 15:41:56 +0530

    splash screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -23,10 +23,17 @@ class SplashScreen extends StatelessWidget {
         height: MediaQuery.sizeOf(context).height,
         child: Column(children: [
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 350, horizontal: 80),
+            padding: EdgeInsets.symmetric(
+                vertical: MediaQuery.sizeOf(context).height * 0.38,
+                horizontal: 80),
             child: Image(image: AssetImage("asset/images/Glogo.png")),
           )
         ]),
+      ),
+      bottomNavigationBar: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height * 0.13,
+        child: Image(image: AssetImage("asset/images/logo.png")),
       ),
     );
   }
